c91a1668580f424cebcff1bccc2aee4ff93aee0d
Jeroen Vervaeke <jeroenvervaeke@users.noreply.github.com>
2021-11-21 20:46:23 +0100

    Started traits rework

--- src/lib.rs
+++ src/lib.rs
@@ -1,155 +1,158 @@
-//! # redux - A Rust implementation of Redux.
-//!
-//! Redux provides a clean way of managing states in an application.
-//! It could be user data such as preferences or information about the state of the program.
-//!
-//! ## Concepts
-//!
-//! In Redux data is immutable. The only way to change it is to take it and create some new data by following a set of rules.
-//!
-//! ### State
-//!
-//! A state is the form of data Redux manages. Theoretically it can be anything, but for an easy explanation let's take the following example:
-//! We have a simple counter application. It does nothing more than counting.
-//! Our state would look the following:
-//!
-//! ```
-//! #[derive(Default)]
-//! struct State {
-//!     counter: i8
-//! }
-//! ```
-//!
-//! ### Actions
-//!
-//! To change the state we need to dispatch actions. In Rust, they would usually be represented by an enum.
-//! For the counter, we want to increment and decrement it.
-//!
-//! ```
-//! enum Action {
-//!     Increment,
-//!     Decrement
-//! }
-//! ```
-//!
-//! ### Reducer
-//!
-//! To actually change the state (read: create a new one), we need what is called a reducer.
-//! It is a simple function which takes in the current state plus the action to perform and returns a new state.
-//!
-//! ```
-//! # struct State {
-//! #     counter: i8
-//! # }
-//! #
-//! # enum Action {
-//! #     Increment,
-//! #     Decrement
-//! # }
-//! #
-//! fn reducer(state: &State, action: &Action) -> State {
-//!     match action {
-//!         Action::Increment => State {
-//!             counter: state.counter + 1
-//!         },
-//!         Action::Decrement => State {
-//!             counter: state.counter - 1
-//!         }
-//!     }
-//! }
-//! ```
-//!
-//! Note how the reducer uses the old data to create a new state.
-//!
-//! ### Store
-//!
-//! To put it all together, we use a store which keeps track of a state and provides an easy to use API for dispatching actions.
-//! The store takes the reducer and an initial state.
-//!
-//! ```
-//! # #[derive(Default)]
-//! # struct State {
-//! #     counter: i8
-//! # }
-//! #
-//! # enum Action {
-//! #     Increment,
-//! #     Decrement
-//! # }
-//! #
-//! # fn reducer(state: &State, action: &Action) -> State {
-//! #     match action {
-//! #         Action::Increment => State {
-//! #             counter: state.counter + 1
-//! #         },
-//! #         Action::Decrement => State {
-//! #             counter: state.counter - 1
-//! #         }
-//! #     }
-//! # }
-//! #
-//! // The store needs to be mutable as it will change its inner state when dispatching actions.
-//! let mut store = redux_rs::Store::new(reducer, State::default());
-//!
-//! // Let it do its highly complex math.
-//! store.dispatch(Action::Increment);
-//! store.dispatch(Action::Decrement);
-//!
-//! // Print the current count.
-//! println!("{}", store.state().counter);
-//! ```
-//!
-//! ### Subscriptions
-//!
-//! Sometimes one might want to listen to changes happening. This is where subscriptions come in.
-//! They are callbacks with the current state that get called whenever an action gets dispatched.
-//!
-//! ```
-//! # #[derive(Default)]
-//! # struct State {
-//! #     counter: i8
-//! # }
-//! #
-//! # enum Action {
-//! #     Increment,
-//! #     Decrement
-//! # }
-//! #
-//! # fn reducer(state: &State, action: &Action) -> State {
-//! #     match action {
-//! #         Action::Increment => State {
-//! #             counter: state.counter + 1
-//! #         },
-//! #         Action::Decrement => State {
-//! #             counter: state.counter - 1
-//! #         }
-//! #     }
-//! # }
-//! #
-//! # let mut store = redux_rs::Store::new(reducer, State::default());
-//! #
-//! store.subscribe(|state: &State| {
-//!      println!("Something changed! Current value: {}", state.counter);
-//! });
-//! ```
+use std::marker::PhantomData;
 
-#![cfg_attr(not(feature = "std"), no_std)]
-#![cfg_attr(not(feature = "std"), feature(alloc))]
+pub trait Store<State, Action> {
+    fn state(&self) -> &State;
+    fn dispatch(&mut self, action: Action);
 
-#[cfg(not(feature = "std"))]
-extern crate alloc;
-#[cfg(not(feature = "std"))]
-use alloc::vec::Vec;
-#[cfg(feature = "std")]
-use std::vec::Vec;
+    fn subscribe<S>(&mut self, subscriber: S)
+    where
+        S: StoreSubscriber<State> + 'static;
+}
 
-mod middleware;
-mod reducer;
-mod store;
-mod subscription;
+pub trait StoreSubscriber<State> {
+    fn updated(&mut self, state: &State);
+}
 
-pub use middleware::Middleware;
-pub use reducer::Reducer;
-#[cfg(not(feature = "devtools"))]
-pub use store::Store;
-pub use subscription::Subscription;
+impl<S, State> StoreSubscriber<State> for S
+where
+    S: FnMut(&State),
+{
+    fn updated(&mut self, state: &State) {
+        self(state)
+    }
+}
+
+pub trait Reducer<State, Action> {
+    fn reduce(&self, state: State, action: Action) -> State;
+}
+
+impl<R, State, Action> Reducer<State, Action> for R
+where
+    R: Fn(State, Action) -> State,
+{
+    fn reduce(&self, state: State, action: Action) -> State {
+        self(state, action)
+    }
+}
+
+pub struct StoreBase<State, Action, Reducer>
+where
+    Reducer: crate::Reducer<State, Action>,
+{
+    state: Option<State>,
+    subscribers: Vec<Box<dyn StoreSubscriber<State>>>,
+    reducer: Reducer,
+    _types: PhantomData<Action>,
+}
+
+impl<State, Action, Reducer> StoreBase<State, Action, Reducer>
+where
+    Reducer: crate::Reducer<State, Action>,
+{
+    pub fn with_default_state(reducer: Reducer) -> Self
+    where
+        State: Default,
+    {
+        StoreBase {
+            state: Some(Default::default()),
+            subscribers: Default::default(),
+            reducer,
+            _types: Default::default(),
+        }
+    }
+}
+
+impl<State, Action, Reducer> Store<State, Action> for StoreBase<State, Action, Reducer>
+where
+    Reducer: crate::Reducer<State, Action>,
+{
+    fn state(&self) -> &State {
+        // We can always unwrap here,
+        // state is an Option because I didn't want to use unsafe code in the dispatch method
+        &self.state.as_ref().unwrap()
+    }
+
+    fn dispatch(&mut self, action: Action) {
+        // We can always unwrap here, state is only None when calculating the new state
+        // Since we have a mutual reference this won't cause any further issues
+        let old_state = self.state.take().unwrap();
+        let new_state = self.reducer.reduce(old_state, action);
+
+        // Set the new calculated state
+        self.state = Some(new_state);
+
+        // Let all subscribers know that the store updated
+        for subscriber in &mut self.subscribers {
+            subscriber.updated(&self.state.as_ref().unwrap());
+        }
+    }
+
+    fn subscribe<S>(&mut self, subscriber: S)
+    where
+        S: StoreSubscriber<State> + 'static,
+    {
+        self.subscribers.push(Box::new(subscriber));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::atomic::{AtomicI32, Ordering};
+    use std::sync::Arc;
+
+    enum CounterAction {
+        Up,
+        Down,
+    }
+
+    #[derive(Default, Debug, PartialEq)]
+    struct Counter {
+        value: i32,
+    }
+
+    #[test]
+    fn base_example() {
+        let mut store =
+            StoreBase::with_default_state(|state: Counter, action: CounterAction| match action {
+                CounterAction::Up => Counter {
+                    value: state.value + 1,
+                    ..state
+                },
+                CounterAction::Down => Counter {
+                    value: state.value - 1,
+                    ..state
+                },
+            });
+
+        let unit_test_value = Arc::new(AtomicI32::new(0));
+
+        let unit_test_value_1 = unit_test_value.clone();
+        store.subscribe(move |state: &Counter| {
+            unit_test_value_1.fetch_add(state.value, Ordering::Relaxed);
+        });
+
+        let unit_test_value_2 = unit_test_value.clone();
+        store.subscribe(move |state: &Counter| {
+            unit_test_value_2.fetch_add(state.value, Ordering::Relaxed);
+        });
+
+        // Test initial state
+        assert_eq!(&Counter { value: 0 }, store.state());
+
+        // Count up
+        store.dispatch(CounterAction::Up);
+        assert_eq!(&Counter { value: 1 }, store.state());
+
+        // Count up
+        store.dispatch(CounterAction::Up);
+        assert_eq!(&Counter { value: 2 }, store.state());
+
+        // Count down
+        store.dispatch(CounterAction::Down);
+        assert_eq!(&Counter { value: 1 }, store.state());
+
+        // Test subscriber state
+        assert_eq!(8, unit_test_value.load(Ordering::Relaxed));
+    }
+}
