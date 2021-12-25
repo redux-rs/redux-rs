46a22272f07a60268bf53ebb873a37a18e319a64
Jeroen Vervaeke <jeroenvervaeke@users.noreply.github.com>
2021-12-25 22:39:25 +0100

    (WIP) Reworked library so store.dispatch is immutable, middleware is still missing

--- src/lib.rs
+++ src/lib.rs
@@ -1,158 +1,213 @@
+use log::warn;
+use std::any::Any;
 use std::marker::PhantomData;
+use std::sync::mpsc::{channel, Sender};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::thread::JoinHandle;
 
-pub trait Store<State, Action> {
-    fn state(&self) -> &State;
-    fn dispatch(&mut self, action: Action);
+type DynamicAction = Box<dyn Any + Send + 'static>;
 
-    fn subscribe<S>(&mut self, subscriber: S)
-    where
-        S: StoreSubscriber<State> + 'static;
+pub trait Dispatch {
+    fn dispatch(&self, action: DynamicAction);
 }
 
-pub trait StoreSubscriber<State> {
-    fn updated(&mut self, state: &State);
-}
-
-impl<S, State> StoreSubscriber<State> for S
-where
-    S: FnMut(&State),
-{
-    fn updated(&mut self, state: &State) {
-        self(state)
-    }
+pub trait WithState<State> {
+    fn state(&self) -> State;
 }
 
 pub trait Reducer<State, Action> {
     fn reduce(&self, state: State, action: Action) -> State;
 }
 
-impl<R, State, Action> Reducer<State, Action> for R
+impl<F, State, Action> Reducer<State, Action> for F
 where
-    R: Fn(State, Action) -> State,
+    F: Fn(State, Action) -> State
 {
     fn reduce(&self, state: State, action: Action) -> State {
         self(state, action)
     }
 }
 
-pub struct StoreBase<State, Action, Reducer>
+pub trait Subscriber<State>: Send {
+    fn updated(&self, state: &State);
+}
+
+impl<F, State> Subscriber<State> for F
 where
-    Reducer: crate::Reducer<State, Action>,
+    F: for<'r> Fn(&'r State) + Send
 {
+    fn updated(&self, state: &State) {
+        self(state)
+    }
+}
+
+pub struct Store<State, Action> {
+    data: Arc<Mutex<StoreData<State>>>,
+
+    action_sender: Sender<DynamicAction>,
+
+    _join_handle: JoinHandle<()>,
+    _types: PhantomData<Action>
+}
+
+struct StoreData<State> {
     state: Option<State>,
-    subscribers: Vec<Box<dyn StoreSubscriber<State>>>,
-    reducer: Reducer,
-    _types: PhantomData<Action>,
+    subscribers: Vec<Box<dyn Subscriber<State>>>
 }
 
-impl<State, Action, Reducer> StoreBase<State, Action, Reducer>
-where
-    Reducer: crate::Reducer<State, Action>,
-{
-    pub fn with_default_state(reducer: Reducer) -> Self
+impl<State, Action> Store<State, Action> {
+    pub fn new<RootReducer>(reducer: RootReducer) -> Self
     where
-        State: Default,
+        Action: 'static,
+        RootReducer: Reducer<State, Action> + Send + 'static,
+        State: Default + Send + 'static
     {
-        StoreBase {
-            state: Some(Default::default()),
-            subscribers: Default::default(),
-            reducer,
-            _types: Default::default(),
-        }
+        Self::new_with_state(reducer, State::default())
+    }
+
+    pub fn new_with_state<RootReducer>(reducer: RootReducer, default_state: State) -> Self
+    where
+        Action: 'static,
+        RootReducer: Reducer<State, Action> + Send + 'static,
+        State: Send + 'static
+    {
+        let (sender, receiver): (Sender<DynamicAction>, _) = channel();
+
+        let data = Arc::new(Mutex::new(StoreData {
+            state: Some(default_state),
+            subscribers: Vec::new()
+        }));
+
+        let thread_data = data.clone();
+        let join_handle = thread::spawn(move || {
+            while let Ok(action) = receiver.recv() {
+                let action = match action.downcast() {
+                    Ok(action) => *action,
+                    Err(_) => {
+                        warn!("Action should be of the same type of the store. You're probably missing middleware to handle the kind of action you've send to the store!");
+                        continue;
+                    }
+                };
+
+                let mut data_lock = thread_data
+                    .lock()
+                    .expect("Only returns an error when a previous piece of code paniced");
+
+                let old_state = data_lock.state.take().unwrap();
+                let new_state = reducer.reduce(old_state, action);
+
+                data_lock.state = Some(new_state);
+
+                let state = data_lock.state.as_ref().unwrap();
+                data_lock.subscribers.iter().for_each(|s| s.updated(state));
+            }
+        });
+
+        let store = Self {
+            data,
+
+            action_sender: sender,
+
+            _join_handle: join_handle,
+            _types: Default::default()
+        };
+
+        store
+    }
+
+    pub fn subscribe(&mut self, subscriber: Box<dyn Subscriber<State>>) {
+        self.data.lock().unwrap().subscribers.push(subscriber);
     }
 }
 
-impl<State, Action, Reducer> Store<State, Action> for StoreBase<State, Action, Reducer>
+impl<State, Action> Dispatch for Store<State, Action> {
+    fn dispatch(&self, action: DynamicAction) {
+        // This only fails when the receiver is gone (inner store) this should never happen
+        let _ = self.action_sender.send(action);
+    }
+}
+
+impl<State, Action> WithState<State> for Store<State, Action>
 where
-    Reducer: crate::Reducer<State, Action>,
+    State: Clone
 {
-    fn state(&self) -> &State {
-        // We can always unwrap here,
-        // state is an Option because I didn't want to use unsafe code in the dispatch method
-        &self.state.as_ref().unwrap()
-    }
-
-    fn dispatch(&mut self, action: Action) {
-        // We can always unwrap here, state is only None when calculating the new state
-        // Since we have a mutual reference this won't cause any further issues
-        let old_state = self.state.take().unwrap();
-        let new_state = self.reducer.reduce(old_state, action);
-
-        // Set the new calculated state
-        self.state = Some(new_state);
-
-        // Let all subscribers know that the store updated
-        for subscriber in &mut self.subscribers {
-            subscriber.updated(&self.state.as_ref().unwrap());
-        }
-    }
-
-    fn subscribe<S>(&mut self, subscriber: S)
-    where
-        S: StoreSubscriber<State> + 'static,
-    {
-        self.subscribers.push(Box::new(subscriber));
+    fn state(&self) -> State {
+        self.data
+            .lock()
+            .expect("Only returns an error when a previous piece of code paniced")
+            .state
+            .as_ref()
+            .expect(
+                "State never contains None, only during updates, which are behind this very mutex"
+            )
+            .clone()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::sync::atomic::{AtomicI32, Ordering};
-    use std::sync::Arc;
+    use std::sync::{
+        atomic::{AtomicU8, Ordering},
+        Barrier
+    };
+
+    #[derive(Clone, Default, Debug, PartialEq)]
+    struct CounterStore(i32);
 
     enum CounterAction {
-        Up,
-        Down,
+        Increment,
+        Decrement
     }
 
-    #[derive(Default, Debug, PartialEq)]
-    struct Counter {
-        value: i32,
+    fn counter_reducer(state: CounterStore, action: CounterAction) -> CounterStore {
+        let value = state.0;
+
+        let new_value = match action {
+            CounterAction::Increment => value + 1,
+            CounterAction::Decrement => value - 1
+        };
+
+        CounterStore(new_value)
     }
 
     #[test]
-    fn base_example() {
-        let mut store =
-            StoreBase::with_default_state(|state: Counter, action: CounterAction| match action {
-                CounterAction::Up => Counter {
-                    value: state.value + 1,
-                    ..state
-                },
-                CounterAction::Down => Counter {
-                    value: state.value - 1,
-                    ..state
-                },
-            });
+    fn counter_without_middleware() {
+        let (sender, receiver) = channel();
 
-        let unit_test_value = Arc::new(AtomicI32::new(0));
+        let mut store = Store::new(counter_reducer);
+        store.subscribe(Box::new(move |state: &CounterStore| {
+            sender.send(state.0).unwrap();
+        }));
 
-        let unit_test_value_1 = unit_test_value.clone();
-        store.subscribe(move |state: &Counter| {
-            unit_test_value_1.fetch_add(state.value, Ordering::Relaxed);
-        });
+        store.dispatch(Box::new(CounterAction::Increment));
+        store.dispatch(Box::new(CounterAction::Decrement));
 
-        let unit_test_value_2 = unit_test_value.clone();
-        store.subscribe(move |state: &Counter| {
-            unit_test_value_2.fetch_add(state.value, Ordering::Relaxed);
-        });
+        assert_eq!(1, receiver.recv().unwrap());
+        assert_eq!(0, receiver.recv().unwrap());
+    }
 
-        // Test initial state
-        assert_eq!(&Counter { value: 0 }, store.state());
+    #[test]
+    fn counter_send_invalid_action() {
+        let number_of_updates = Arc::new(AtomicU8::new(0));
+        let incremented = Arc::new(Barrier::new(2));
 
-        // Count up
-        store.dispatch(CounterAction::Up);
-        assert_eq!(&Counter { value: 1 }, store.state());
+        let mut store = Store::new(counter_reducer);
 
-        // Count up
-        store.dispatch(CounterAction::Up);
-        assert_eq!(&Counter { value: 2 }, store.state());
+        let subscriber_number_of_updates = number_of_updates.clone();
+        let subscriber_barrier = incremented.clone();
+        store.subscribe(Box::new(move |state: &CounterStore| {
+            subscriber_number_of_updates.fetch_add(1, Ordering::Relaxed);
+            if state.0 == 1 {
+                subscriber_barrier.wait();
+            }
+        }));
 
-        // Count down
-        store.dispatch(CounterAction::Down);
-        assert_eq!(&Counter { value: 1 }, store.state());
+        store.dispatch(Box::new("test"));
+        store.dispatch(Box::new(CounterAction::Increment));
 
-        // Test subscriber state
-        assert_eq!(8, unit_test_value.load(Ordering::Relaxed));
+        incremented.wait();
+        assert_eq!(1, number_of_updates.load(Ordering::Relaxed));
     }
 }
