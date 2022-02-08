6713c950805cdd51e446956e3a98539d878f7cfd
Jeroen Vervaeke <jeroenvervaeke@users.noreply.github.com>
2022-02-09 00:05:36 +0100

    Split library in separate files

--- src/reducer.rs
+++ src/reducer.rs
@@ -45,7 +45,7 @@ pub trait Reducer<State, Action> {
 
 impl<F, State, Action> Reducer<State, Action> for F
 where
-    F: Fn(State, Action) -> State,
+    F: Fn(State, Action) -> State
 {
     fn reduce(&self, state: State, action: Action) -> State {
         self(state, action)
