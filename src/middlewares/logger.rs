43b6bb704859515ec1d8e6d323854c4dfe859242
Jeroen Vervaeke <jeroenvervaeke@users.noreply.github.com>
2022-02-24 23:15:50 +0100

    Documented the logger middleware

--- src/middlewares/logger.rs
+++ src/middlewares/logger.rs
@@ -4,11 +4,33 @@ use log::{log, Level};
 use std::fmt::Debug;
 use std::sync::Arc;
 
+/// A middleware which logs every single action that has been dispatched to the store
+/// We're using the `log` crate to achieve the logging, the log level can be set
+///
+/// ## Usage:
+/// ```
+/// # #[derive(Default)]
+/// # struct EmptyStore;
+/// #
+/// # #[derive(Debug)]
+/// # struct LogableAction(&'static str);
+/// #
+/// # fn reducer(store: EmptyStore, _action: LogableAction) -> EmptyStore {
+/// #     store
+/// # }
+/// // Setup the logger middleware with default "Debug" log level
+/// let logger_middleware = LoggerMiddleware::new(Level::Debug);
+///
+/// // Create a new store and wrap it with the logger middleware
+/// let store = Store::new(reducer).wrap(logger_middleware).await;
+/// ```
 pub struct LoggerMiddleware {
     log_level: Level,
 }
 
 impl LoggerMiddleware {
+    /// Crate a new logger.
+    /// LogLevel is the level that the logs will be output with
     pub fn new(log_level: Level) -> Self {
         LoggerMiddleware { log_level }
     }
@@ -24,7 +46,10 @@ where
     where
         Inner: StoreApi<State, Action> + Send + Sync,
     {
+        // Log the action
         log!(self.log_level, "Action: {:?}", action);
+
+        // Continue dispatching the action
         inner.dispatch(action).await
     }
 }
