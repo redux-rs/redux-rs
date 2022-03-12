31626727e84744e7244567d7e8c5bb25ffd34eab
Jeroen Vervaeke <jeroenvervaeke@users.noreply.github.com>
2022-03-12 23:45:33 +0100

    Documented thunk middleware

--- src/middlewares/thunk.rs
+++ src/middlewares/thunk.rs
@@ -2,6 +2,93 @@ use crate::{MiddleWare, StoreApi};
 use async_trait::async_trait;
 use std::sync::Arc;
 
+/// # Thunk middleware
+/// Thunk middleware enables us to introduce side-effects in a redux application.
+///
+/// With this middleware you can dispatch actions and thunks to your store.
+///
+/// ## Trait example
+/// ```
+/// use async_trait::async_trait;
+/// use std::sync::Arc;
+/// use std::time::Duration;
+/// use redux_rs::{Store, StoreApi};
+/// use redux_rs::middlewares::thunk::{ActionOrThunk, ThunkMiddleware, Thunk};
+/// use tokio::time::sleep;
+///
+/// #[derive(Default, Debug, PartialEq)]
+/// struct UserState {
+///     users: Vec<User>,
+/// }
+///
+/// #[derive(Clone, Debug, PartialEq)]
+/// struct User {
+///     id: u8,
+///     name: String,
+/// }
+///
+/// enum UserAction {
+///     UsersLoaded { users: Vec<User> },
+/// }
+///
+/// fn user_reducer(state: UserState, action: UserAction) -> UserState {
+///     match action {
+///         UserAction::UsersLoaded { users } => UserState { users },
+///     }
+/// }
+///
+/// struct LoadUsersThunk;
+/// #[async_trait]
+/// impl<Api> Thunk<UserState, UserAction, Api> for LoadUsersThunk
+///     where
+///         Api: StoreApi<UserState, UserAction> + Send + Sync + 'static,
+/// {
+///     async fn execute(&self, store_api: Arc<Api>) {
+///         // Emulate api call by delaying for 100 ms
+///         sleep(Duration::from_millis(100)).await;
+///
+///         // Return the data to the store
+///         store_api
+///             .dispatch(UserAction::UsersLoaded {
+///                 users: vec![
+///                     User {
+///                         id: 0,
+///                         name: "John Doe".to_string(),
+///                     },
+///                     User {
+///                         id: 1,
+///                         name: "Jane Doe".to_string(),
+///                     },
+///                 ],
+///             })
+///             .await;
+///     }
+/// }
+/// # async fn async_test() {
+/// let store = Store::new(user_reducer).wrap(ThunkMiddleware).await;
+/// store.dispatch(ActionOrThunk::Thunk(Box::new(LoadUsersThunk))).await;
+///
+/// let users = store.select(|state: &UserState| state.users.clone()).await;
+/// assert_eq!(users, vec![]);
+///
+/// sleep(Duration::from_millis(200)).await;
+///
+/// let users = store.select(|state: &UserState| state.users.clone()).await;
+/// assert_eq!(
+///     users,
+///     vec![
+///         User {
+///             id: 0,
+///             name: "John Doe".to_string(),
+///         },
+///         User {
+///             id: 1,
+///             name: "Jane Doe".to_string(),
+///         },
+///     ]
+/// );
+/// # }
+/// ```
 pub struct ThunkMiddleware;
 
 #[async_trait]
